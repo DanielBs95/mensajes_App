6f1db742f6ea55cb447ea1da9520d0368b12d10a
Daniel Barrantes <cleysenenjinia@hotmail.com>
2022-10-13 19:43:52 -0500

    Se crea un Menu

--- src/main/java/com/mycompany/mavenproject1/Inicio.java
+++ src/main/java/com/mycompany/mavenproject1/Inicio.java
@@ -1,21 +1,55 @@
 package com.mycompany.mavenproject1;
+
+import static com.mycompany.mavenproject1.MensajesService.*;
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
  * @author ohmyfi
  */
 public class Inicio {
-    
-    public static void main(String[] args){
-    
-       Conexion conexion = new Conexion();
-       
-       try(Connection cnx = conexion.get_connection()){
-           
-       }catch(Exception e) {
-           System.out.println(e);
-       }
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        Conexion conexion = new Conexion();
+        int opcion = 0;
+        do {
+            System.out.println("----------------");
+            System.out.println(" Aplicacion de Mensajes");
+            System.out.println("1. Crear un mensaje");
+            System.out.println("2. Listar mensaje");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Eliminar mensaje");
+            System.out.println("5. Salir");
+            //Aqui usuario eligue opcion
+            System.out.println("Eliga una opcion");
+            opcion = Integer.parseInt(teclado.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    crearMensaje();
+                    break;
+                case 2:
+                    listarMensajes();
+                    break;
+                case 3:
+                    editarMensaje();
+                    break;
+                case 4:
+                    borrarMensajes();
+                    break;
+                default:
+                    break;
+            }
+        } while (opcion != 5);
+
+        try (Connection cnx = conexion.get_connection()) {
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-    
+
 }
